17ec3e55728e6c7c834f3db1e8756c9f72a17883
chamaedorea-productions <96385448+chamaedorea-productions@users.noreply.github.com>
2023-02-01 14:32:08 +0100

    Fixed imports and initialization
    Updated imports
    Changed how the "user" class is initialized
    Moved old code to "old"

--- changable_settings.py
+++ changable_settings.py
@@ -3,7 +3,8 @@ PORT = 5000
 # HOST = "192.168.0.252"
 # HOST = "192.168.1.39"
 # HOST = "127.0.0.1"
+HOST = "192.168.1.22"
 
 # <<< IPv4 on Windows >>> 
-import socket
-HOST = socket.gethostbyname(socket.gethostname())
+# import socket
+# HOST = socket.gethostbyname(socket.gethostname())
--- core/_user.py
+++ core/_user.py
@@ -4,7 +4,11 @@ from dataclasses import dataclass
 
 @dataclass
 class User():
-    unique_ids: list[str] = list()
+    unique_ids: list[str]
     """All the unique ids associated with the user."""
-    reported_words: list[int] = list()
+    reported_words: list[int]
     """Times (dates) when the user reported a word."""
+
+    def __init__(self):
+        self.unique_ids = list()
+        self.reported_words = list()
