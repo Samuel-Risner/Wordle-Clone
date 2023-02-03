aaebd2882f4bb9f2fd1188292f86cbda170b46c7
chamaedorea-productions <96385448+chamaedorea-productions@users.noreply.github.com>
2023-02-03 20:36:11 +0100

    Added documentation comments

--- ts/game/colour.ts
+++ ts/game/colour.ts
@@ -1,5 +1,10 @@
 export { colour_key, colour_letter };
 
+/**
+ * Colours an element curresponding to the number.
+ * @param element The element that is supposed to be coloured.
+ * @param num The number for the colour.
+ */
 function _colour_element(element: HTMLElement, num: number): void {
     if (num == 0) {
         element.className = "";
@@ -10,10 +15,20 @@ function _colour_element(element: HTMLElement, num: number): void {
     }
 }
 
+/**
+ * Colours a letter on the game field.
+ * @param letter_element The tiles element.
+ * @param num The number for the colour.
+ */
 function colour_letter(letter_element: HTMLDivElement, num: number): void {
     _colour_element(letter_element, num);
 }
 
+/**
+ * Colours a letter on the keyboard.
+ * @param key The letters character.
+ * @param num The number for the colour.
+ */
 function colour_key(key: string, num: number): void {
     let element = document.getElementById(key) as HTMLButtonElement;
 
